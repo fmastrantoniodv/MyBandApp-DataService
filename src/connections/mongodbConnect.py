9a0940d0e2bf1d02aef420441a8b7a494bb819fc
fmastrantoniodv <franco.mastrantonio@davinci.edu.ar>
2022-10-27 23:06:21 -0300

    base mongoDB conectada

---
+++ src/connections/mongodbConnect.py
@@ -0,0 +1,61 @@
+import json
+from flask import Flask, request, jsonify
+from flask_mongoengine import MongoEngine
+
+app = Flask(__name__)
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'mybandapp',
+    'host': 'mongodb+srv://mybandappfrankieadmin:mybandtesting777@atlascluster.22fk4nm.mongodb.net/?retryWrites=true&w=majority',
+    'port': 5010
+}
+db = MongoEngine()
+db.init_app(app)
+
+class User(db.Document):
+    name = db.StringField()
+    email = db.StringField()
+    def to_json(self):
+        return {"name": self.name,
+                "email": self.email}
+
+@app.route('/', methods=['GET'])
+def query_records():
+    name = request.args.get('name')
+    user = User.objects(name=name).first()
+    if not user:
+        return jsonify({'error': 'data not found'})
+    else:
+        return jsonify(user.to_json())
+
+@app.route('/', methods=['PUT'])
+def create_record():
+    record = json.loads(request.data)
+    print(record)
+    user = User(name=record['name'],
+                email=record['email'])
+    user.save()
+    return jsonify(user.to_json())
+
+@app.route('/', methods=['POST'])
+def update_record():
+    record = json.loads(request.data)
+    print(record)
+    user = User.objects(name=record['name']).first()
+    if not user:
+        return jsonify({'error': 'data not found'})
+    else:
+        user.update(email=record['email'])
+    return jsonify(user.to_json())
+
+@app.route('/', methods=['DELETE'])
+def delete_record():
+    record = json.loads(request.data)
+    user = User.objects(name=record['name']).first()
+    if not user:
+        return jsonify({'error': 'data not found'})
+    else:
+        user.delete()
+    return jsonify(user.to_json())
+
+if __name__ == "__main__":
+    app.run(debug=True)
